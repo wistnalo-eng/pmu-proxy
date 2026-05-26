b60ca75380ebf946123a63ef25f2a21ef7c2ed07
wistnalo-eng <wistnalo@gmail.com>
2026-05-26 18:52:21 +0200

    fix partants

--- api/partants.js
+++ api/partants.js
@@ -4,11 +4,13 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
 
-  const { date } = req.query;
-  if (!date) return res.status(400).json({ error: "date manquante (DDMMYYYY)" });
+  const { date, reunion, course } = req.query;
+  if (!date || !reunion || !course) {
+    return res.status(400).json({ error: "Paramètres manquants: date, reunion, course" });
+  }
 
   try {
-    const url = `https://offline.turfinfo.api.pmu.fr/rest/client/7/programme/${date}`;
+    const url = `https://offline.turfinfo.api.pmu.fr/rest/client/7/programme/${date}/R${reunion}/C${course}/participants?specialisation=OFFLINE`;
     const r = await fetch(url, {
       headers: {
         "Accept": "application/json",
